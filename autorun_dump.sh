2d642ae6e961b4dd29a272d5cad9e3c4d9e63ba7
borisdblade <boris@dashlocal.com>
2018-06-26 13:22:56 +0200

    root dir more abstract getting

--- autorun_dump.sh
+++ autorun_dump.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-AUTO_URI=$(readlink -f "$0")
-AUTO_EXTRACT=/autorun_dump.sh
-AUTO_ROOTDIR=${AUTO_URI%${AUTO_EXTRACT}}
+AUTO_ROOTDIR=$(cd "$(dirname "$0")"; pwd)
 
 bash ${AUTO_ROOTDIR}/dbdump.sh prod --list local -i --dt
