4b2131897cff9dcc99edf2055c9d0be4701f8681
borisdblade <boris@dashlocal.com>
2018-06-26 11:35:20 +0200

    remove echo

--- autorun_dump.sh
+++ autorun_dump.sh
@@ -4,7 +4,4 @@ AUTO_URI=$(readlink -f "$0")
 AUTO_EXTRACT=/autorun_dump.sh
 AUTO_ROOTDIR=${AUTO_URI%${AUTO_EXTRACT}}
 
-# config
-echo ${AUTO_ROOTDIR}/config.sh
-
 bash ${AUTO_ROOTDIR}/dbdump.sh prod --list local -i --dt
